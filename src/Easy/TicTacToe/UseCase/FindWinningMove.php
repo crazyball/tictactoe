9f438f85fccdd459c8498aa1aa9a5dc2b6ea9c4f
Loïc Ambrosini <crazyball@crazyball.fr>
2020-07-07 23:36:37 +0200

    5e test passé

--- src/Easy/TicTacToe/UseCase/FindWinningMove.php
+++ src/Easy/TicTacToe/UseCase/FindWinningMove.php
@@ -21,6 +21,17 @@ class FindWinningMove
             }
         }
 
+        $board = [array_column($board, 0), array_column($board, 1), array_column($board, 2)];
+
+        foreach ($board as &$row) {
+            $countSigns = array_count_values($row);
+            if (array_key_exists('O', $countSigns) && $countSigns['O'] === 2) {
+                $row[array_search('.', $row)] = 'O';
+            }
+        }
+
+        $board = [array_column($board, 0), array_column($board, 1), array_column($board, 2)];
+
         return $board;
     }
 }
--- src/Easy/TicTacToe/tic_tac_toe.feature
+++ src/Easy/TicTacToe/tic_tac_toe.feature
@@ -39,3 +39,13 @@ Feature: Tic Tac Toe
       | O | O | O |
       | . | . | . |
       | . | . | . |
+  Scenario: No opponent - Column
+    Given find the winning move of O player
+    When the following tic tac toe board is :
+      | O | . | . |
+      | . | . | . |
+      | O | . | . |
+    Then the final tic tac toe board is :
+      | O | . | . |
+      | O | . | . |
+      | O | . | . |
